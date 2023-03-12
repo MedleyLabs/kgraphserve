047712b2bacb2305835dd34dd4b6c28a0dcc5d3e
eric-yates <eric@medleyagency.com>
2023-03-12 13:30:42 -0700

    Create setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='kgraphserve',
+    version='0.0.1',
+    description='A Flask app for serving knowledge graphs',
+    author='Eric Yates',
+    author_email='eric@medleyagency.com',
+    url='https://github.com/MedleyLabs/kgraphserve',
+    packages=['kgraphserve'],
+    install_requires=[
+        'flask',
+        'flask_cors',
+        'owlready2',
+        'wget'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Flask',
+        'Framework :: Pytest',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
